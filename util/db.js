31862053ab3a547f513a3052bd70764e0ba9f6a4
Serhii Mamedov <bboyvivus@gmail.com>
2020-01-29 00:13:55 +0200

    impoved connection string

--- util/db.js
+++ util/db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const client = new MongoClient(process.env.DB_URL, { useUnifiedTopology: true });
+const client = new MongoClient(process.env.DB_URL || 'mongodb://localhost:27017/docgen', { useUnifiedTopology: true });
 let mongoConnection;
 
 const createMongoConnection = async () => {
