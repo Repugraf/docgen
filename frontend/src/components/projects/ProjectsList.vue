b0a87e476bd5096490ae6c3ea0f918d8d11a8337
serhii mamedov <serhiy.ma@starkey.co.il>
2020-01-30 15:38:25 +0200

    frontend setup of projects

--- frontend/src/components/projects/ProjectsList.vue
+++ frontend/src/components/projects/ProjectsList.vue
@@ -1,19 +1,32 @@
 <template>
-  <div>
-    <pre v-if="projects">{{projects}}</pre>
+  <div v-if="projects">
+    <div
+      v-for="item of projects"
+      :key="item._id"
+      @click="setCurrentProject(item)"
+      style="cursor:pointer;"
+    >
+      <span style="color: blue;">{{item.name}}</span>
+    </div>
+    <pre>{{projects}}</pre>
   </div>
 </template>
 
 <script>
 export default {
-computed: {
+  computed: {
     projects() {
       return this.$store.state.projects.projectList;
     }
   },
-}
+  methods: {
+    setCurrentProject(project) {
+      if (!project) return;
+      this.$store.commit("projects/setCurrentProject", project);
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
